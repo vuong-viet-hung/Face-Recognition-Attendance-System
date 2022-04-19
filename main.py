fbbd46633404b18d5183becfcf010f15aee103c3
Vuong Viet Hung <vuongviethung156@gmail.com>
2022-04-19 09:30:59 +0700

    Change regex to match id and confidence

--- main.py
+++ main.py
@@ -2,27 +2,25 @@ import csv
 import datetime
 import logging
 import re
-from typing import List
-import serial.tools.list_ports
+from typing import Match, Tuple
+import serial
 
 
 USB_PORT = "/dev/ttyUSB0"
-BAUNDRATE = 9600
 
 
-def info(packet: str) -> List[str]:
-    return [datetime.datetime.now()] + re.findall(r"\d+", packet)
+def info(found_id_match: Match[str]) -> Tuple[str]:
+    time = str(datetime.datetime.now())
+    found_id = found_id_match.group(1)
+    confidence = found_id_match.group(2)
+    return (time, found_id, confidence)
 
 
 def main() -> None:
 
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
+    found_id_regex = re.compile(r"Found ID #(\d*) with confidence of (\d*)")
 
-    found_id_regex = re.compile(r"Found ID #(\d)*")
-
-    with serial.Serial(USB_PORT, BAUNDRATE) as serial_port, open(
+    with serial.Serial(USB_PORT, baudrate=9600) as serial_port, open(
         "matches.csv", "a"
     ) as matches:
 
@@ -39,9 +37,14 @@ def main() -> None:
             found_id_match = found_id_regex.search(packet)
             if found_id_match is None:
                 continue
-            # Save match finger id info
-            csv_writer.writerow(info(packet))
+            # Write match finger id info
+            csv_writer.writerow(info(found_id_match))
 
 
 if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
     main()
